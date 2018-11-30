6108148d17a8a110ee3ef2b58eb02b1c0f463e78
samahetfield <samaniego@correo.ugr.es>
2018-11-30 16:11:51 +0100

    Modificados test. Closes #8

--- test/test-app.js
+++ test/test-app.js
@@ -1,5 +1,5 @@
 var request = require('supertest'),
-				app = require('../bot.js');
+				app = require('../SerieService.js');
 
 
 var SerieClass = require('../SerieClass');
@@ -18,7 +18,53 @@ var SerieClass = require('../SerieClass');
 							.expect('Content-Type', 'application/json')
 							.expect(200, done);
 					});
+				});
 
+				describe( "PUT and DELETE Functionality", function(){
+					it('Put new serie', function(done){
+						request(app)
+							.put('/series_favoritas/Test')
+							.expect(200,done);
+					});
+
+					it('Delete favourite serie', function(done){
+						request(app)
+							.delete('/series_favoritas/Test')
+							.expect(200, done);
+					});
+
+					it('Delete not exist serie', function(done){
+						request(app)
+							.delete('/series_favoritas/SerieQueNoExiste')
+							.expect(404, done);
+					});
+
+				});
+
+				describe( "GET Functionality", function() {
+
+					it('responds with last serie added on /series_favoritas/lastadded', function(done){
+						var sc = new SerieClass();
+						
+						sc.addserie("test");
+						var lastserie = sc.lastfavourite();
+
+						if(lastserie == "test")
+							done();
+
+					});
+
+					it('Show last removed', function(done){
+						var sc = new SerieClass();
+
+						sc.addserie("test");
+						sc.deleteserie("test");
+
+						var lastdeleted = sc.lastdeleted();
+
+						if(lastdeleted == "test")
+							done();
+					})
 				});
 
 
