96879cafb570f5ee30aab63f0b1ca4830729f180
samahetfield <samaniego@correo.ugr.es>
2018-12-17 11:07:53 +0100

    Añado automatización. Closes #17

---
+++ acopio.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+REGION="westeurope"
+
+echo "Script de automatización para creación de una máquina virtual en Azure."
+
+echo "Realice el login"
+
+#Hacemos login en azure
+az login
+
+
+echo "----------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------"
+
+echo "¿Es la primera vez que ejecuta el script? [Y/N]".
+
+read FIRSTTIME
+
+if [ $FIRSTTIME = "Y" ] || [ $FIRSTTIME = "y" ]
+then
+	echo "Vamos a crear un grupo de recursos para alojar la máquina"
+	az group create --name MvProjectCC --location $REGION 
+fi	
+
+
+echo "----------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------"
+
+echo "Creación de la Máquina Virtual"
+
+create_output=$(az vm create -g MvProjectCC -n vmHito4 --image Canonical:UbuntuServer:18.04-LTS:latest --generate-ssh-keys)
+
+# Obtenemos la IP pública de nuestra máquina virtual creada
+Public_IP=$(echo $create_output | jq -r '.publicIpAddress')
+
+echo "La IP de tu máquina virtual creada es $Public_IP"
+
+# Abrimos el puerto 80 para poder acceder a la MV desde el navegador
+az vm open-port --resource-group MvProjectCC --name vmHito4 --port 80
+
+
+#Ejecutamos ansible para el provisionamiento de la MV
+
+ansible-playbook ./provision/ansible.yml --inventory "$Public_IP,"
+
+
+
